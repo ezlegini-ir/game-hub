64d3f52dd2a1ae8be8f327f9dc6ff7b0c3e29261
ezlegini-ir <alireza.ezlegini@yahoo.com>
2024-10-15 21:53:03 +0330

    Get optimized Images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+
+const getCroppedImage = (url: string) => {
+  const target = 'media/'
+  const index = url.indexOf(target) + target.length;
+  const cropSize = 'crop/600/400/'
+  return url.slice(0, index) + cropSize + url.slice(index)
+}
+
+export default getCroppedImage
