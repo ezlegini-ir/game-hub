bb383955c4b638cc7b36e8b70df1c9e53296e95c
ezlegini-ir <alireza.ezlegini@yahoo.com>
2024-10-15 09:32:18 +0330

    Add fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Button } from "@chakra-ui/react";
 import React from "react";
 
 const App = () => {
-  return <Button colorScheme="blue">Blue Button</Button>;
+  return (
+    <>
+      <Button colorScheme="blue">Blue Button</Button>
+    </>
+  );
 };
 
 export default App;
