5dc999d44a06db1c2ca8583dfd448596c0bc9098
ezlegini-ir <alireza.ezlegini@yahoo.com>
2024-10-15 21:06:17 +0330

    Displaying Platform Icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaLinux,
+  FaApple,
+  FaXbox,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    linux: FaLinux,
+    mac: FaApple,
+    xbox: FaXbox,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <Icon color={"gray.500"} as={iconMap[platform.slug]} />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,12 +3,17 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 //INTERFACES
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[]
   }
-
   interface FetchGameResponse {
     count: number;
     results: Game[];
