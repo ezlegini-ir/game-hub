755fdec399f118c11695758baff1149e502fa09b
ezlegini-ir <alireza.ezlegini@yahoo.com>
2024-10-16 06:56:11 +0330

    Filter Games By Platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,6 +18,6 @@ export interface Game {
 
   
 // COMPONENT FUNCTION
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre])
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id, platforms: selectedPlatform?.id}}, [selectedGenre?.id, selectedPlatform?.id])
 
 export default useGames
