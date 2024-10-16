f444419eb56fcd329a7801b920d6a59375c9e2e2
ezlegini-ir <alireza.ezlegini@yahoo.com>
2024-10-16 09:03:00 +0330

    Build Search Input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Color Mode</Text>
+      <Text whiteSpace={"nowrap"}>Color Mode</Text>
     </HStack>
   );
 };
