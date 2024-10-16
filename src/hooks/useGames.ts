409753bde5740dc14c1666b98cc4c146d30a754f
ezlegini-ir <alireza.ezlegini@yahoo.com>
2024-10-16 09:56:22 +0330

    Add Emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,10 +15,18 @@ export interface Game {
     background_image: string;
     parent_platforms: {platform: Platform}[]
     metacritic: number;
+    rating_top : number;
   }
 
   
 // COMPONENT FUNCTION
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText}}, [gameQuery])
+const useGames = (gameQuery: GameQuery) => useData<Game>('/games',
+  {params: {genres: gameQuery.genre?.id,
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder,
+    search: gameQuery.searchText,
+  
+  }},
+    [gameQuery])
 
 export default useGames
